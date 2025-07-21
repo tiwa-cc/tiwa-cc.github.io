767b0ef5a9697d11b3429fbac01087bebe2df68c
tiwa-cc <cc.tiwa@gmail.com>
2025-07-21 22:17:22 +0900

    feat: add breakpointSizeProvider

--- lib/shared/parts/breakpoint.dart
+++ lib/shared/parts/breakpoint.dart
@@ -1,6 +1,59 @@
-enum Breakpoint { xs, sm, md, lg, xl }
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// A notifier that tracks the current breakpoint size based on the screen width.
+class BreakpointSizeNotifier extends StateNotifier<BreakpointSize> {
+  BreakpointSizeNotifier() : super(BreakpointSize(Size.zero, Breakpoint.xs));
+
+  void update(Size size) {
+    state = BreakpointSize(
+      size,
+      Breakpoint.values.firstWhere(
+        (breakpoint) => breakpoint.inRange(size.width),
+      ),
+    );
+  }
+}
+
+/// Represents a size and its corresponding breakpoint.
+class BreakpointSize {
+  /// Creates a BreakpointSize with the given size and breakpoint.
+  final Size size;
+
+  /// The breakpoint that corresponds to the size.
+  final Breakpoint breakpoint;
+
+  /// Constructs a BreakpointSize with the specified size and breakpoint.
+  BreakpointSize(this.size, this.breakpoint);
+
+  bool get isXs => breakpoint == Breakpoint.xs;
+  bool get isSm => breakpoint == Breakpoint.sm;
+  bool get isMd => breakpoint == Breakpoint.md;
+  bool get isLg => breakpoint == Breakpoint.lg;
+  bool get isXl => breakpoint == Breakpoint.xl;
+}
+
+/// An enum representing different breakpoints for responsive design.
+enum Breakpoint {
+  /// Extra small devices (phones, 0 - 600px).
+  xs,
+
+  /// Small devices (tablets, 600 - 905px).
+  sm,
+
+  /// Medium devices (small laptops, 905 - 1240px).
+  md,
+
+  /// Large devices (desktops, 1240 - 1440px).
+  lg,
+
+  /// Extra large devices (large desktops, 1440px and above).
+  xl,
+}
+
+/// Extension methods for Breakpoint to provide min and max width values.
 extension BreakpointExt on Breakpoint {
+  /// Returns the minimum width for the breakpoint.
   double min() {
     return switch (this) {
       Breakpoint.xs => 0,
@@ -11,6 +64,7 @@ extension BreakpointExt on Breakpoint {
     };
   }
 
+  /// Returns the maximum width for the breakpoint.
   double max() {
     return switch (this) {
       Breakpoint.xs => 600,
@@ -21,6 +75,7 @@ extension BreakpointExt on Breakpoint {
     };
   }
 
+  /// Checks if a given width falls within the range of the breakpoint.
   bool inRange(double width) {
     return min() <= width && width < max();
   }
--- lib/shared/providers.dart
+++ lib/shared/providers.dart
@@ -1,1 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_portfolio/shared/parts/breakpoint.dart';
 
+/// A provider that holds the current breakpoint size.
+final breakpointSizeProvider = StateNotifierProvider(
+  (ref) => BreakpointSizeNotifier(),
+);
