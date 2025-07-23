d194696dd054a2e42b75e5fdc23d4fd2c6421ce6
tiwa-cc <cc.tiwa@gmail.com>
2025-07-24 03:02:32 +0900

    feat: add future experience

---
+++ lib/features/experience/experience_panel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/l10n/l10n.dart';
+import 'package:my_portfolio/shared/widgets/card_title.dart';
+
+class ExperiencePanel extends StatelessWidget {
+  final VoidCallback onHeader;
+
+  const ExperiencePanel({super.key, required this.onHeader});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context)!;
+    final title = l10n.experienceTitle;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // ScrollToHeadButton(onHeader: onHeader),
+        CardTitle(title: title),
+        ...List.generate(50, (index) => Text("experience")),
+      ],
+    );
+  }
+}
